bb1516d373226faffc445713c93e96e1b629dec3
Zafer Can <104571337+zaforcan@users.noreply.github.com>
2023-11-16 20:51:11 +0300

    Update classifypose.py

--- classifypose.py
+++ classifypose.py
@@ -5,26 +5,9 @@ import mediapipe as mp
 mp_pose = mp.solutions.pose
 
 def classifyPose(landmarks, output_image, display=False):
-    '''
-    This function classifies yoga poses depending upon the angles of various body joints.
-    Args:
-        landmarks: A list of detected landmarks of the person whose pose needs to be classified.
-        output_image: A image of the person with the detected pose landmarks drawn.
-        display: A boolean value that is if set to true the function displays the resultant image with the pose label 
-        written on it and returns nothing.
-    Returns:
-        output_image: The image with the detected pose landmarks drawn and pose label written.
-        label: The classified pose label of the person in the output_image.
-
-    '''
     
     # Initialize the label of the pose. It is not known at this stage.
-    label = 'Unknown Pose'
-
-    left_elbow_angle = "bilinmeyen"
-
-    # Specify the color (Red) with which the label will be written on the image.
-    color = (0, 0, 255)
+    label = 'Unknown'
     
     # Calculate the required angles.
     #----------------------------------------------------------------------------------------------------------------
@@ -57,80 +40,9 @@ def classifyPose(landmarks, output_image, display=False):
     # Get the angle between the right hip, knee and ankle points 
     right_knee_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value],
                                       landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value],
-                                      landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value])
-    
-    #----------------------------------------------------------------------------------------------------------------
-    
-    # Check if it is the warrior II pose or the T pose.
-    # As for both of them, both arms should be straight and shoulders should be at the specific angle.
-    #----------------------------------------------------------------------------------------------------------------
-    
-    # Check if the both arms are straight.
-    if left_elbow_angle > 165 and left_elbow_angle < 195 and right_elbow_angle > 165 and right_elbow_angle < 195:
-
-        # Check if shoulders are at the required angle.
-        if left_shoulder_angle > 80 and left_shoulder_angle < 110 and right_shoulder_angle > 80 and right_shoulder_angle < 110:
-
-    # Check if it is the warrior II pose.
-    #----------------------------------------------------------------------------------------------------------------
-
-            # Check if one leg is straight.
-            if left_knee_angle > 165 and left_knee_angle < 195 or right_knee_angle > 165 and right_knee_angle < 195:
-
-                # Check if the other leg is bended at the required angle.
-                if left_knee_angle > 90 and left_knee_angle < 120 or right_knee_angle > 90 and right_knee_angle < 120:
-
-                    # Specify the label of the pose that is Warrior II pose.
-                    label = 'Warrior II Pose' 
-                        
-    #----------------------------------------------------------------------------------------------------------------
-    
-    # Check if it is the T pose.
-    #----------------------------------------------------------------------------------------------------------------
-    
-            # Check if both legs are straight
-            if left_knee_angle > 160 and left_knee_angle < 195 and right_knee_angle > 160 and right_knee_angle < 195:
-
-                # Specify the label of the pose that is tree pose.
-                label = 'T Pose'
-
-    #----------------------------------------------------------------------------------------------------------------
-    
-    # Check if it is the tree pose.
-    #----------------------------------------------------------------------------------------------------------------
-    
-    # Check if one leg is straight
-    if left_knee_angle > 165 and left_knee_angle < 195 or right_knee_angle > 165 and right_knee_angle < 195:
-
-        # Check if the other leg is bended at the required angle.
-        if left_knee_angle > 315 and left_knee_angle < 335 or right_knee_angle > 25 and right_knee_angle < 45:
-
-            # Specify the label of the pose that is tree pose.
-            label = 'Tree Pose'
-                
-    #----------------------------------------------------------------------------------------------------------------
-    
-    # Check if the pose is classified successfully
-    if label != 'Unknown Pose':
+                                      landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value])            
         
-        # Update the color (to green) with which the label will be written on the image.
-        color = (0, 255, 0)  
-    
-    # Write the label on the output image. 
-    # cv2.putText(output_image, label, (10, 30),cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
-    
-    # Check if the resultant image is specified to be displayed.
-    if display:
-
-        print("pose klasifikasyonu görüntülendi")
-    
-        # Display the resultant image.
-        # plt.figure(figsize=[10,10])
-        # plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
-        
-    else:
-        # Return the output image and the classified label.
-        return output_image, label
+    return output_image, label
     
 
 
